d22c041ad1720a8a9741ce1e923f73420d82cd20
Aaron <mohammad.chegini@hotmail.com>
2020-02-09 14:37:30 +0330

    some progress

--- driver.py
+++ driver.py
@@ -4,15 +4,24 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from time import sleep
 
-
 driver = webdriver.Chrome(
     executable_path="C:\\Users\\Aaron\\Desktop\\Webdriver\\chromedriver.exe")
 
-# driver.maximize_window()
 driver.get("https://golestan.sbu.ac.ir/Forms/AuthenticateUser/main.htm")
 
 seq = driver.find_elements_by_tag_name('iframe')
-
 print("No of frames present in the web page are: ", len(seq))
+# switching between the iframes based on index
+for index in range(len(seq)):
+    driver.switch_to.default_content
+    iframe = driver.find_elements_by_tag_name('iframe')[index]
+    driver.switch_to.frame(iframe)
+    driver.implicitly_wait(30)
+    driver.save_screenshot("screenshot.png")
+    # highlight the contents of the selected iframe
+    something = driver.find_element_by_tag_name('frame')
+    print(something)
+
+driver.switch_to.default_content
 
 driver.quit()
